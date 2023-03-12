77e0867c4b68c14505fc66c47d1c64239ea4cff7
yvanruben <rubens.wdg@gmail.com>
2023-03-12 22:37:06 +0400

    wk4_dx

---
+++ wk4/forloop-classex.js
@@ -0,0 +1,21 @@
+const value=15;
+
+for (let n = 0; n <=value; n++) {
+    const element = value[index];
+    console.log(value)
+    
+}
+
+
+
+
+let names=['john', 'sarah',23,'rudolf',34];
+for (let i =0 1< names.length; i++) {
+   if (typeof name[i] !=='string')
+   {
+    break
+   }
+    
+}
+
+
---
+++ wk4/wk4_dx.js
@@ -0,0 +1,24 @@
+let value=6;
+let star="*";
+
+
+let str="";
+
+
+
+
+for (let count=0; count < value; count++){
+    // console.log(count)
+    const line=''.padEnd(count+1,'*');
+    //console.log(line);
+
+    console.log(str += '*');
+}
+   
+
+// while (value<=6){
+//       if value {}
+//     console.log(star);
+//     value++;
+    
+// }
